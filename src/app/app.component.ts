7fd036ab2dab6c191c34e9d6b72c2c0f97e1158a
Javier Montes Carrera <jav_mc@yahoo.es>
2024-02-19 00:52:49 -0500

    feat: Integrar el wallet-adapter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { HdWalletMultiButtonComponent } from '@heavy-duty/wallet-adapter-material';
 
 @Component({
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, HdWalletMultiButtonComponent],
   selector: 'demo-root',
   template: `
-    <header>
-      <h1>Hola, soy Javier.</h1>
+    <header class="py-7">
+      <h1 class="text-3xl text-center mb-2">Hola, soy Javier.</h1>
+      <div class="flex justify-center">
+        <hd-wallet-multi-button></hd-wallet-multi-button>
+      </div>
     </header>
   `,
 })
